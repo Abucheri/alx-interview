871193487fe74ed8a1aeb229caa6b842b67a80c3
O'Brien <abucheriobrien@gmail.com>
2024-05-03 02:51:43 +0300

    Log parsing update_9

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This script continuously reads from sys.stdin, processes each line,
+and prints statistics for every 10 lines or upon receiving a
+keyboard interrupt signal (CTRL + C).
+"""
 
 import sys
 
@@ -27,13 +32,21 @@ status_code_counts = {
 }
 
 try:
+    # Process each line from sys.stdin
     for line in sys.stdin:
+        # split line into words using split() method, and then reverse
+        # using [::-1] so that the last word appears first.
         read_line = line.split()
         read_line = read_line[::-1]
 
+        # If the number of words in the line is greater than 2,
+        # it means it's a valid log entry
         if len(read_line) > 2:
-            line_count += 1
+            line_count += 1  # Then increment by 1
 
+            # If line_count is less than or equal to 10
+            # file size and status code of the current line are updated in
+            # total_file_size and status_code_counts dictionary respectively
             if line_count <= 10:
                 total_file_size += int(read_line[0])
                 status_code = read_line[1]
@@ -41,6 +54,9 @@ try:
                 if (status_code in status_code_counts.keys()):
                     status_code_counts[status_code] += 1
 
+            # If line_count becomes 10, the current statistics are printed
+            # using the print_statistics function, and line_count
+            # is reset to 0.
             if (line_count == 10):
                 print_statistics(status_code_counts, total_file_size)
                 line_count = 0
