9bf6ef8cead76191ee3a46bc66cf52fd1658b6eb
O'Brien <abucheriobrien@gmail.com>
2024-05-02 23:52:17 +0300

    Log parsing update_0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,58 +2,57 @@
 
 import sys
 import re
-import signal
+from collections import OrderedDict
+from datetime import datetime
 
 
 # Initialize variables to store metrics
 total_file_size = 0
-status_code_counts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
+status_code_counts = OrderedDict.fromkeys(
+        [200, 301, 400, 401, 403, 404, 405, 500], 0)
 line_count = 0
 
 
 def print_statistics():
-    """Prints the computed statistics."""
+    """Prints current totals of file size and status code counts."""
     print("File size:", total_file_size)
-    for status_code, count in sorted(status_code_counts.items()):
+    for code, count in status_code_counts.items():
         if count > 0:
-            print("{}: {}".format(status_code, count))
+            print("{}: {}".format(code, count))
 
 
-def signal_handler(sig, frame):
-    """Handler for keyboard interruption (CTRL + C)."""
-    print_statistics()
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
-for line in sys.stdin:
-    # Parse log entry using regular expression
-    match = re.match(r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[([^]]*)\]'
-                     r'"GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)$',
-                     line.strip())
-    if not line:
-        # Break out of the loop if end of input stream is reached
-        break
-    if match:
-        # Extract relevant information from the log entry
-        ip_address, _, _, status_code, file_size = match.groups()
-
-        # Update metrics
-        total_file_size += int(file_size)
-        status_code = int(status_code)
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
-
+try:
+    for line in sys.stdin:
         line_count += 1
-        if line_count % 10 == 0:
-            print_statistics()
+
+        # Parse log entry using regular expression
+        match = re.match(r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\]'
+                         r'"GET /projects/260 HTTP/1\.1" (\d+) (\d+)$',
+                         line.strip())
+        if match:
+            # Extract relevant information from the log entry
+            ip_address, timestamp, status_code, file_size = match.groups()
+
+            # Check timestamp format
+            try:
+                datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
+            except ValueError:
+                sys.stderr.write("{}: {}: invalid timestamp format\n"
+                                 .format(sys.argv[0], line_count))
+
+            # Check URL
+            if status_code != '200':
+                sys.stderr.write("{}: {}: unexpected HTTP status code\n"
+                                 .format(sys.argv[0], line_count))
+
+            # Update metrics
+            total_file_size += int(file_size)
+            status_code = int(status_code)
+            if status_code in status_code_counts:
+                status_code_counts[status_code] += 1
+
+            if line_count % 10 == 0:
+                print_statistics()
+except KeyboardInterrupt:
+    # Handle keyboard interruption (CTRL + C)
+    print_statistics()
